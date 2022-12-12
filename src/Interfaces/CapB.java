f5a2af5ea58076788e6f85d9337fef10ff8d1174
DELL <DELL@DESKTOP-O123K8O>
2022-12-12 03:33:09 -0400

    DOC: PROYECTO SIN ERRORES
    POR: IVAN

--- src/Interfaces/CapB.java
+++ src/Interfaces/CapB.java
@@ -4,7 +4,7 @@ package Interfaces;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-
+ 
 
 import java.awt.Graphics;
 import java.awt.Image;
